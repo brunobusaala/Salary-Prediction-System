8f5d09ef273c6b708c68ffda2ea9ac3cc668e728
Bruno Busaala <brunobusaala@gmail.com>
2023-09-25 17:08:48 +0300

    minor fixes

--- my_ml_model.py
+++ my_ml_model.py
@@ -1,9 +1,14 @@
-# my_ml_model.py
-
 import joblib
+import json
+import sys
+
 
 def load_model(model_path):
     return joblib.load(model_path)
 
+
 def predict(model, input_data):
-    return model.predict(input_data)
+    prediction = model.predict(input_data)
+    result = {"PredictedSalary": prediction.tolist()}
+    print(json.dumps(result))  # Print the JSON result
+    sys.stdout.flush()  # Flush the output
